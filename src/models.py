0add0c94894efd75fd9ae7d36e6824d394c89d96
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2023-12-18 19:14:38 +0000

    starwars data modeling missing a last review

--- src/models.py
+++ src/models.py
@@ -1,32 +1,49 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
+    email = Column(String(250), nullable=False, unique=True)
+
+    favorites = relationship("Favorites", back_populates="user")
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    description = Column(String(500))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    description = Column(String(500))
+    galaxy = Column(String(250))
 
-    def to_dict(self):
-        return {}
+    characters = relationship("Character", back_populates="planet")
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id', ondelete="SET NULL"), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id', ondelete="SET NULL"), nullable=True)
+
+    character = relationship("Character", back_populates="favorites")
+    planet = relationship("Planet", back_populates="favorites")
+
+    __table_args__ = (
+        PrimaryKeyConstraint('character_id', 'planet_id'),
+    )
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
